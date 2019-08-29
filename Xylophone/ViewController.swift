fc0f01ea6931260317c1d988b37a0b594a206220
Emerson Javid Gonzalez Morales <emegonza@Emersons-MacBook-Pro.local>
2019-08-29 08:48:00 -0500

    First commit and work the app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var xylophoneSoundEffect: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 1:
+            playSound(soundName: "note1");
+        case 2:
+            playSound(soundName: "note2");
+        case 3:
+            playSound(soundName: "note3");
+        case 4:
+            playSound(soundName: "note4");
+        case 5:
+            playSound(soundName: "note5");
+        case 6:
+            playSound(soundName: "note6");
+        case 7:
+            playSound(soundName: "note7");
+        default:
+            print("sound no exits")
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let path = Bundle.main.path(forResource: soundName, ofType: "wav")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            xylophoneSoundEffect = try AVAudioPlayer(contentsOf: url)
+            xylophoneSoundEffect?.play()
+        } catch {
+            print("error play the sound:: \(error)")
+        }
+    }
 }
 
